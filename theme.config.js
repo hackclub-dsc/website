9aae11717f50887ea81bed10e429752ae4dc5f19
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-03-07 09:07:57 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © DSC Hack Club.
-      <a href="Slack">Join Us!</a>
+      <a href="https://hackclub.com/Slack">Join Us!</a>
       <style jsx>{`
         a {
           float: right;
