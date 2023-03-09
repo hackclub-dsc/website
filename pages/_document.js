b1a801ebbe7b2364418d0cdc6f4987f1e00007d9
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2023-03-09 17:07:28 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Hack Club, Dhupguri Science Club',
+    description: 'The Dhupguri Science Club Hack Club website!',
+    image: 'https://dsc.hackclub.com/images/header.png'
   }
 
   return (
